bb4d1eedec7f0615e4e814e525e6f96bd871a588
MBP <davidkingmushi@gmail.com>
2024-05-20 18:25:22 +0300

    added client routes for admin

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -188,6 +188,25 @@ const router = createRouter({
       component: () => import('../views/admin/users/UserView.vue')
     },
     {
+      path: '/admin/clients/all',
+      name: 'adminClientsAll',
+      meta: { requiresAuth: true },
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/admin/clients/AllClients.vue')
+    },
+
+    {
+      path: '/admin/clients/new',
+      name: 'adminClientsNew',
+      meta: { requiresAuth: true },
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/admin/clients/NewClient.vue')
+    },
+    {
       path: '/admin/profile',
       name: 'adminViewProfile',
       meta: { requiresAuth: true },
